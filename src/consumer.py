754b97e57f0867539270e5fcc5a263c850d92b9b
panggggg <p.picczy@gmail.com>
2021-12-29 15:32:04 +0700

    Update callback of consumer

--- src/consumer.py
+++ src/consumer.py
@@ -19,18 +19,22 @@ mongodb._connect()
 print(f"[ Connected to MongoDB ]")
 
 redis_config = config["redis_config"]
-redis_connect = redis.Redis(host=redis_config["host"], port=redis_config["port"], password=redis_config["password"])
+redis_connect = redis.Redis(
+    host=redis_config["host"], port=redis_config["port"], password=redis_config["password"])
 print(f"[ Connected to Redis ]")
 
 credentials = pika.PlainCredentials("root", "root")
-connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost", credentials=credentials))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host="localhost", credentials=credentials))
 channel = connection.channel()
 
 channel.exchange_declare(exchange="py-homework", exchange_type="direct")
 
+
 def callback(ch, method, properties, body):
     message = json.loads(body)
     title = message.get("title")
+    key = title.replace(" ", "")
     print("Receive message: ")
     print(f"""
             Title: {message.get("title")}
@@ -38,10 +42,18 @@ def callback(ch, method, properties, body):
             Authors: {message.get("authors")}
             Categories: {message.get("categories")}
         """)
-    print("Save to redis", redis_connect.set(title, title))
+
+    redis = {}
+    redis["title"] = message.get("title")
+    redis["pageCount"] = message.get("pageCount")
+    redis["authors"] = message.get("authors")
+    redis["categories"] = message.get("categories")
+
+    redis_connect.set(key, str(redis))
     mongodb.save_to_mongo(message)
     print("Successfully saved!")
     channel.basic_ack(delivery_tag=method.delivery_tag)
+
 
 channel.basic_consume(queue="Test", on_message_callback=callback)
 print("[*] Waitting for data")
