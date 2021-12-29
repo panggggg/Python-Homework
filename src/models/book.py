e8c9205be1e4ae9311a8e519b6c8ddb1ea4907ac
panggggg <p.picczy@gmail.com>
2021-12-29 15:42:34 +0700

    Add UpdateBook

--- src/models/book.py
+++ src/models/book.py
@@ -1,9 +1,18 @@
-from pydantic import BaseModel
+from datetime import datetime
+from pydantic import BaseModel, Field
+from typing import Optional
 
-class BookModel(BaseModel):
+class CreateBook(BaseModel):
     title: str
     pageCount: int
     authors: str
     categories: str
-    created_at: str
-    updated_at: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+class UpdateBook(BaseModel):
+    title: Optional[str]
+    pageCount: Optional[int]
+    authors: Optional[str]
+    categories: Optional[str]
+    updated_at: datetime = Field(default_factory=datetime.now)
