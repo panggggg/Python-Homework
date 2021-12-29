2ad575443c2f6b0d12352385401a7c55d07147e0
panggggg <p.picczy@gmail.com>
2021-12-29 15:41:48 +0700

    Update check message before send to consumer

--- src/publisher.py
+++ src/publisher.py
@@ -1,7 +1,9 @@
-import arrow
 import json
 import pika
+import redis
 from typing import Dict
+
+from Config.development import config
 
 credentials = pika.PlainCredentials("root", "root")
 connection = pika.BlockingConnection(
@@ -14,6 +16,10 @@ channel.queue_declare(queue="Test")
 channel.queue_bind(exchange="py-homework",
                    queue="Test", routing_key="Test-key")
 
+redis_config = config["redis_config"]
+redis_connect = redis.Redis(
+    host=redis_config["host"], port=redis_config["port"], password=redis_config["password"])
+print(f"[ Connected to Redis ]")
 
 def get_input_meassage() -> Dict:
     message = {}
@@ -30,17 +36,26 @@ def get_input_meassage() -> Dict:
     message["pageCount"] = pageCount
     message["authors"] = authors
     message["categories"] = categories
-    message["created_at"] = str(arrow.utcnow())
-    message["updated_at"] = str(arrow.utcnow())
 
     return message
 
-print(type(get_input_meassage))
-body = get_input_meassage()
-print(body)
+#NOTE: loads(dict -> str), dumps(str -> dict)
 
-channel.basic_publish(exchange="py-homework",
-                      routing_key="Test-key", body=json.dumps(body))
-print("[.] Message has send")
+body = get_input_meassage()
+message = json.dumps(body)
+message_dict = json.loads(message)
+print(str(message_dict))
+get_title = message_dict.get("title")
+key = get_title.replace(" ", "")
+
+get_from_redis = redis_connect.get(key)
+message_from_redis = get_from_redis.decode("utf-8")
+
+if str(message_dict) == message_from_redis:
+    print("This message was sent")
+else:
+    channel.basic_publish(exchange="py-homework",
+                            routing_key="Test-key", body=json.dumps(body))
+    print("[.] Message has send")
 
 channel.close()
