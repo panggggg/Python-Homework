ebe471ad0587ce72b73a84e06571e959998ef674
panggggg <p.picczy@gmail.com>
2022-01-06 17:58:37 +0700

    Add rabbimq_config

--- src/Config/development.py
+++ src/Config/development.py
@@ -12,5 +12,10 @@ config = {
         "host": "localhost",
         "port": 6379,
         "password": "root"
+    },
+    "rabbitmq_config": {
+        "username": "root",
+        "password": "root",
+        "host": "localhost"
     }
 }
